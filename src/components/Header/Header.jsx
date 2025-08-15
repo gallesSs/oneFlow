f26932b464a82d620b1396e08880aceb75dc3b2b
Arthur <agavrik464@gmail.com>
2025-08-15 13:14:16 +0300

    header done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import logo from "/public/Header/logo.svg";
+import lang from "/public/Header/lang.svg";
+
+const Header = () => {
+  return (
+    <header className="flex font-sans py-5 px-4 justify-between">
+      <a className="ml-3 cursor-pointer" href="/">
+        <img src={logo} alt="" />
+      </a>
+      <div className="flex justify-center items-center">
+        <nav>
+          <ul className="flex justify-center items-center gap-8 text-[#013A4C] leading-5 mr-8">
+            <li className="mr-[30px]">
+              <a href="">Why Oneflow?</a>
+            </li>
+            <li className="mr-[30px]">
+              <a href="">Learn</a>
+            </li>
+            <li>
+              <a href="">Pricing</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Blog</a>
+            </li>
+          </ul>
+        </nav>
+        <ul className="flex justify-center items-center gap-2">
+          <li>
+            <button className="w-[114px] cursor-pointer rounded-[4px] border border-[#FFD063] h-10 bg-[#FFD063]">
+              Get a demo
+            </button>
+          </li>
+          <li>
+            <button className="h-10 cursor-pointer w-[75px] border border-[#346170] rounded-[4px]">
+              Log in
+            </button>
+          </li>
+        </ul>
+        <img className="ml-12" src={lang} alt="" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
